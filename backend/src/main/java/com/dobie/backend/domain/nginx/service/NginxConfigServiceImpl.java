671ab906b1e23fcef8b556c45ce09510b9d53bda
유호정 <ys0403ab@naver.com>
2024-05-03 14:07:17 +0900

    FIX: 파일저장 경로 수정

--- backend/src/main/java/com/dobie/backend/domain/nginx/service/NginxConfigServiceImpl.java
+++ backend/src/main/java/com/dobie/backend/domain/nginx/service/NginxConfigServiceImpl.java
@@ -80,15 +80,15 @@ public class NginxConfigServiceImpl implements NginxConfigService{
         }else{
             sb.append(withoutHttpsConfig(nginxConfig)); //https 미사용시 config파일 생성
         }
-     
+
         String fileName = projectName + ".conf"; //파일이름 [projectName].conf로 만들어주기
-        fileManager.saveFile("/home/ubuntu/nginx",fileName,sb.toString()); //fileManager활용해서 /nginx경로에 저장하기
+        fileManager.saveFile("/nginx",fileName,sb.toString()); //fileManager활용해서 /nginx경로에 저장하기
     }
 
     //프론트 nginx config 파일 생성 후 /[projectName]/[frontendPath]/conf/conf.d 파일에 default.conf이름으로 저장
     @Override
     public void saveFrontNginxConfigFile(String path, String projectName) throws IOException {
-        String frontPath = "./" + projectName + path; //파일 저장할 경로 생성
+        String frontPath = "/" + projectName + path; //파일 저장할 경로 생성
         //해당 파일 경로 이미 있는지 확인
         if (!new File(frontPath).exists()) {
             log.info("잘못된 경로입니다.");
